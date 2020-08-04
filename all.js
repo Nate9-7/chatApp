6d1f82a3c9d80bebc28f3530a3cec7ef9bab2838
Nate9-7 <69191395+Nate9-7@users.noreply.github.com>
2020-08-04 10:33:22 +0100

    Add files via upload

---
+++ all.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyA6bET7aQzYea-okfW1VJaFgGZvXXLvNVU",
+  authDomain: "chatapp-14f46.firebaseapp.com",
+  databaseURL: "https://chatapp-14f46.firebaseio.com",
+  projectId: "chatapp-14f46",
+  storageBucket: "chatapp-14f46.appspot.com",
+  messagingSenderId: "173337952553",
+  appId: "1:173337952553:web:c8359cc8fe5fbc988cf738"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
